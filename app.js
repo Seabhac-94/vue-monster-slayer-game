09eaa20a5e5f8bc4bcc642f33d930cecb89c517a
Zac Allen <zac.allen@outlook.com>
2021-07-13 09:39:16 +0200

    added special attack and restricted to every 3 rounds

--- app.js
+++ app.js
@@ -6,21 +6,40 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         };
+    },
+    computed: {
+        monsterBarStyles(){
+            return {width: this.monsterHealth + '%'}
+        },
+        playerBarStyles(){
+            return {width: this.playerHealth + '%'}
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !== 0
+        }
     },
     methods: {
         attackMonster() {
+            this.currentRound ++;
             const attackValue = getRandomValue(5,12)
             this.monsterHealth -= attackValue;
             this.attackPlayer(); // triggers attackPlayer() once attackMonster is complete
-            console.log(this.monsterHealth)
         },
         attackPlayer(){
             const attackValue = getRandomValue(8,15)
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound ++;
+            const attackValue = getRandomValue(10,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
-    }
+    },
+
 
 })
 
